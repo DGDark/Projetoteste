12a0836d9a4cec6d304b75f04698d195b613e376
DGDark <davipereiraneto71@outlook.com>
2020-01-14 15:53:37 -0300

    pessoa adicicionada

--- src/Teste.java
+++ src/Teste.java
@@ -1,10 +1,11 @@
 public class Teste {
     public static void main(String[] args) {
-        String[] pessoas = new String[4];
+        String[] pessoas = new String[5];
         pessoas[0] = "davi";
         pessoas[1] = "bruno";
         pessoas[2] = "marcelo";
         pessoas[3] = "DGDark";
+        pessoas[4] = "jeff";
 
 //Percorrer a lista e mostrar a quantidade de itens através do contador
         int contador = 0;
