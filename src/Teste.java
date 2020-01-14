18db766f28146e1ef7a2f63fe69f38fe1bfd50d3
DGDark <davipereiraneto71@outlook.com>
2020-01-14 15:46:49 -0300

    subir para o site

---
+++ src/Teste.java
@@ -0,0 +1,21 @@
+public class Teste {
+    public static void main(String[] args) {
+        String[] pessoas = new String[4];
+        pessoas[0] = "davi";
+        pessoas[1] = "bruno";
+        pessoas[2] = "marcelo";
+        pessoas[3] = "DGDark";
+
+//Percorrer a lista e mostrar a quantidade de itens através do contador
+        int contador = 0;
+        for (String pessoa : pessoas){
+            contador++;
+            System.out.println("Item " + contador +":" + pessoa);
+        }
+        System.out.println(pessoas.length);
+
+
+
+    }
+}
+
